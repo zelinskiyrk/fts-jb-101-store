3b846cd67fe7ebf9a8e9d7f77e2e949d89f16c94
Prokurator <fiordi@yandex.ru>
2020-11-17 12:45:16 +0400

    init

---
+++ src/test/java/com/zelinskiyrk/store/StoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelinskiyrk.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
